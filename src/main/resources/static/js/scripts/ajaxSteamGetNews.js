5694a2293948314482639552cf450490e260e147
Ghazkull11 <49782457+Ghazkull11@users.noreply.github.com>
2020-12-14 10:05:03 +0100

    add static files

---
+++ src/main/resources/static/js/scripts/ajaxSteamGetNews.js
@@ -0,0 +1,21 @@
+$("#getNews").click(function(){
+    $.ajax({
+        url: "/api/steam/getNews/" + $("#selectGame").val(),
+        method: 'GET',
+        contentType:'application/json',
+        success: function(result){
+            $("#widgetSteamNews").html(result);
+            newsitems = result["appnews"]["newsitems"];
+            length = newsitems.length;
+            $("#widgetSteamNews").html("");
+            for (var i=1; i<=5; i++) {
+                $("#widgetSteamNews").append("<div id='steamNews'>"+ newsitems[length-i]["contents"]+"</div><hr>");
+            }
+            $("#widgetSteamNews").find("img").attr("class","img-fluid img-thumbnail")
+        },
+        error: function(error){
+            console.log("error");
+            console.log(error.HEADERS_RECEIVED)
+        }
+    });
+});
