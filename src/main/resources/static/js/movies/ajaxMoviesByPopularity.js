c2fdb6fc3db53c0f6bcffecef57e392fed969250
Max <49746475+ZealyMax@users.noreply.github.com>
2020-12-15 11:48:26 +0100

    not valid dates error handled

--- src/main/resources/static/js/movies/ajaxMoviesByPopularity.js
+++ src/main/resources/static/js/movies/ajaxMoviesByPopularity.js
@@ -7,16 +7,21 @@ $("#getMoviesByPopularity").click(function(){
             $("#widgetMoviesByPopularity").html(result);
             console.log(result)
             movieItems = result["results"];
-            console.log(movieItems);
-            for (var i=0; i<=5; i++) {
-                $("#widgetMoviesByPopularity").append("<div class='movie' style='text-align: center'>\
-                                                            <h3><strong>"+ movieItems[i]["original_title"] +"</strong> (Sortie le "+ movieItems[i]["release_date"] +")</h3>\
-                                                            <img class='img-fluid img-thumbnail' style='width: 20%'\
-                                                             src='https://image.tmdb.org/t/p/w500/"+ movieItems[i]["poster_path"]+"'>\
-                                                            <p style='box-sizing: border-box'>"+ movieItems[i]["overview"] +"</p>\
-                                                        </div>\
-                                                        <hr>");
+            if(movieItems[0] === undefined){
+                $("#widgetMoviesByPopularity").html("<p style='color: red'>These are not valid dates, recheck what you wrote</p>");
             }
+            else{
+                for (var i=0; i<=5; i++) {
+                    $("#widgetMoviesByPopularity").append("<div class='movie' style='text-align: center'>\
+                                                                <h3><strong>"+ movieItems[i]["original_title"] +"</strong> (Sortie le "+ movieItems[i]["release_date"] +")</h3>\
+                                                                <img class='img-fluid img-thumbnail' style='width: 20%'\
+                                                                 src='https://image.tmdb.org/t/p/w500/"+ movieItems[i]["poster_path"]+"'>\
+                                                                <p style='box-sizing: border-box'>"+ movieItems[i]["overview"] +"</p>\
+                                                            </div>\
+                                                            <hr>");
+                }
+            }
+            
         
         },
         error: function(error){
