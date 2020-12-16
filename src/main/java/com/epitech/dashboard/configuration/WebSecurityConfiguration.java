61604c2b5bdbdefd3facedfd21cb2a538cb660ea
Ghazkull11 <49782457+Ghazkull11@users.noreply.github.com>
2020-12-16 17:39:13 +0100

    CA FONCTIONNE PAS

--- src/main/java/com/epitech/dashboard/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/epitech/dashboard/configuration/WebSecurityConfiguration.java
@@ -42,16 +42,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/registration").permitAll()
-                .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
-                .authenticated().and().csrf().disable().formLogin()
-                .loginPage("/login").failureUrl("/login?error=true")
+                .anyRequest().authenticated().and().oauth2Login().defaultSuccessUrl("/user/home")
+                .usernameParameter("user_name")
+                .passwordParameter("password")
+                .and().csrf().disable().formLogin()
+                .loginPage("/login?")
+                .failureUrl("/login?error=true")
                 .defaultSuccessUrl("/user/home")
                 .usernameParameter("user_name")
                 .passwordParameter("password")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login").and().exceptionHandling()
-                .accessDeniedPage("/access-denied");
+                .logoutSuccessUrl("/login?").and().exceptionHandling();
+                // */
     }
 
     @Override
--- src/main/java/com/epitech/dashboard/controller/LoginController.java
+++ src/main/java/com/epitech/dashboard/controller/LoginController.java
@@ -19,6 +19,7 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
+
     @RequestMapping(value={"/", "login"}, method = RequestMethod.GET)
     public ModelAndView login(){
         ModelAndView modelAndView = new ModelAndView();
