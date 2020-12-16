282bc1d0c2580bf110d28d96b20a851b924300a2
Max <49746475+ZealyMax@users.noreply.github.com>
2020-12-16 13:18:18 +0100

    better display loveCalc

--- src/main/resources/static/js/LoveCalculator/ajaxLoveGetPercentage.js
+++ src/main/resources/static/js/LoveCalculator/ajaxLoveGetPercentage.js
@@ -1,12 +1,14 @@
 $("#getLove").click(function(){
+    fname = $("#fname").val().replace(" ","");
+    sname = $("#sname").val().replace(" ", "")
     $.ajax({
-        url: "/api/loveCalculator?fname=" + $("#fname").val()+"&sname="+$("#sname").val(),
+        url: "/api/loveCalculator?fname=" + fname+"&sname="+sname,
         method: 'GET',
         contentType:'application/json',
         success: function(result){
             console.log(result);
             $("#widgetLoveCalc").html("");
-            $("#widgetLoveCalc").append(result["result"]);
+            $("#widgetLoveCalc").append(result["percentage"] + "%: " + result["result"]);
         },
         error: function(error){
             console.error("Error : " +error.responseJSON.error + ", status : "+ error.responseJSON.status)
