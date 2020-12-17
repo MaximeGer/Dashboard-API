a32e0a05940fb2e5fe2b503afd1d8ee805e999e4
Bastien <bastien.moinard@epitech.eu>
2020-12-17 14:09:50 +0100

    html modifie

--- src/main/java/com/epitech/dashboard/controller/WeatherController.java
+++ src/main/java/com/epitech/dashboard/controller/WeatherController.java
@@ -15,18 +15,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WeatherController {
-    @RequestMapping(value = "/weather/city/{cityname}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String getFoosBySimplePathWithPathVariable(
-        @PathVariable("cityname") String cityname) throws IOException, InterruptedException {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=525fed8a65a821cff57c8062da27fd8a"))
-                    .build();
+        @RequestMapping(value = "/weather/city/{cityname}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+        @ResponseBody
+        public String getCityMetheo(@PathVariable("cityname") String cityname) throws IOException, InterruptedException {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=525fed8a65a821cff57c8062da27fd8a"))
+                        .build();
     
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+                HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
     
-            return response.body();
+        return response.body();
     }
+    
+        @RequestMapping(value = "/weather/forecast/{cityname}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+        @ResponseBody
+        public String getWeatherForecast(@PathVariable("cityname") String cityname) throws IOException, InterruptedException {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("pro.openweathermap.org/data/2.5/forecast/hourly?q=" + cityname + "&appid=525fed8a65a821cff57c8062da27fd8a"))
+                        .build();
+    
+                HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+    
+        return response.body();
+        }
+    
 }
