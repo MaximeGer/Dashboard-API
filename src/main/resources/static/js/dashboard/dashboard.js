880b42ed0d77ce8d24295f87dea4adf7952e7377
Max <49746475+ZealyMax@users.noreply.github.com>
2020-12-19 00:44:46 +0100

    drag & drop opérationnel

--- src/main/resources/static/js/dashboard/dashboard.js
+++ src/main/resources/static/js/dashboard/dashboard.js
@@ -1,43 +1,70 @@
+var i = 1;
+
 $("body").on("click", "#addWidgetWeather", function (){
     $("#dashboard").append(widgetWeather)
+    console.log($(".widget").last())
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;
 });
 
 
 $("body").on("click","#addWidgetVideoInfos",function (){
     $("#dashboard").append(widgetVideoInfos)
     $(".selectpicker").selectpicker("refresh")
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;i = i +1;
 });
 
 
 $("body").on("click","#addWidgetChannelInfos",function (){
     $("#dashboard").append(widgetChannelInfos)
     $(".selectpicker").selectpicker("refresh")
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;i = i +1;
 });
 
 
 $("body").on("click","#addWidgetSearchedVideoInfos",function (){
     $("#dashboard").append(widgetSearchedVideoInfos)
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;i = i +1;
 });
 
 
 $("body").on("click","#addWidgetSteamNews",function (){
     $("#dashboard").append(widgetSteamNews)
     $(".selectpicker").selectpicker("refresh")
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;i = i +1;
 });
 
 
 $("body").on("click","#addWidgetMoviesByPopularity",function (){
     $("#dashboard").append(widgetMoviesByPopularity)
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;i = i +1;
 });
 
 
 $("body").on("click","#addWidgetSearchMovies",function (){
     $("#dashboard").append(widgetSearchMovies)
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;i = i +1;
 });
 
 
 $("body").on("click","#addWidgetLoveCalc",function (){
     $("#dashboard").append(widgetLoveCalc)
+    $(".widget").last().attr("id", i)
+    $(".widget").children().attr("draggable", "false")
+    i = i+1;i = i +1;
 });
 
 
