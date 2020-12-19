6ebc5344b55dbeac94d936c5599b4781ba715dc5
Ghazkull11 <49782457+Ghazkull11@users.noreply.github.com>
2020-12-19 19:10:41 +0100

    war dans /build/libs/

--- src/main/java/com/epitech/dashboard/DashboardApplication.java
+++ src/main/java/com/epitech/dashboard/DashboardApplication.java
@@ -3,9 +3,17 @@ package com.epitech.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DashboardApplication {
+public class DashboardApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DashboardApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DashboardApplication.class, args);
     }
